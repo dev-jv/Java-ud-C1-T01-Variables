a7f7f879f04d3f3f8389eeba824926564a49e533
dev-jv <dev.javierleon@gmail.com>
2020-12-01 00:29:43 -0500

    one

---
+++ src/Hope.java
@@ -0,0 +1,22 @@
+public class Hope {
+    public static void main(String[] args) {
+        String hope = "Hope!";
+        System.out.println("hope = " + hope);
+        System.out.println("hope.toUpperCase() = " + hope.toUpperCase());
+
+        int n = 10;
+
+        boolean valor = true;
+        int n2 = 5;
+        var n3 = "25";
+        if (valor) {
+            System.out.println("n = " + n);
+            n3 = "15";
+        }
+        System.out.println("n2 = " + n2);
+        System.out.println("n3 = " + n3);
+    }
+}
+
+
+
