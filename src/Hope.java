422a7d93e8ce799dee0e198d145415341d6958be
dev-jv <dev.javierleon@gmail.com>
2020-12-01 00:38:21 -0500

    two

--- src/Hope.java
+++ src/Hope.java
@@ -10,11 +10,21 @@ public class Hope {
         int n2 = 5;
         var n3 = "25";
         if (valor) {
+            n = 25;
             System.out.println("n = " + n);
             n3 = "15";
         }
         System.out.println("n2 = " + n2);
         System.out.println("n3 = " + n3);
+
+        String nombre;
+
+        nombre = "Dee";
+        if(n>10){
+            nombre = "Sam";
+        }
+        System.out.println("n = " + n);
+        System.out.println("nombre = " + nombre);
     }
 }
 
