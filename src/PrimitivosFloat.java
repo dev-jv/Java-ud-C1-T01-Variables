f02931e59426c0b34dd8925f6b39595f3d86d65e
dev-jv <dev.javierleon@gmail.com>
2020-12-02 12:07:51 -0500

    four

---
+++ src/PrimitivosFloat.java
@@ -0,0 +1,11 @@
+public class PrimitivosFloat {
+    public static void main(String[] args) {
+
+//        float realFloat = 15000F;//1.5e4f;
+        float realFloat = 0.00000000015f;//1.5E-10F;//1.5e-10f;
+        System.out.println("realFloat = " + realFloat);
+    }
+}
+
+
+
