89559550a9ffced6190dae1c9259f676cdc6bb65
dev-jv <dev.javierleon@gmail.com>
2020-12-04 00:15:37 -0500

    seven

--- src/PrimitivosChar.java
+++ src/PrimitivosChar.java
@@ -30,6 +30,29 @@ public class PrimitivosChar {
 //        char varDef = 'j';
 
         System.out.println("varDef = " + varDef);
+
+        // ------------------------------- <> Caracteres especiales
+        char espacio = ' ';
+        char espacio2 = '\u0020';
+        System.out.println("Un deshojo es" + espacio + "rojo en" + espacio2 + "Primavera.");
+
+        char retroceso = '\b';
+        System.out.println("Un deshojo es x" + retroceso + "rojo en x\b Primavera.");
+
+        char tabulador = '\t';
+        System.out.println("Un deshojo es" + tabulador + "rojo\ten Primavera.");
+
+        char nuevaLinea = '\n';
+        System.out.println("Un deshojo es" + nuevaLinea + " ..rojo en Primavera.\n Y verde en Verano");
+
+        char retCar = '\r';
+        System.out.println("Un deshojo es" + retCar + "rojo en Primavera.");
+
+        System.out.println("Un deshojo es\n\rrojo en Primavera.");
+
+        System.out.println("Un deshojo es" + System.getProperty("line.separator") + "rojo en Primavera.");
+
+        System.out.println("Un deshojo es" + System.lineSeparator() + "rojo en Primavera.");
     }
 }
 
